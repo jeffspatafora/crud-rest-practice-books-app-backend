df654cdc52774239e74efa0c124b029628082dc6
jeffspatafora <jeffspatafora@gmail.com>
2022-04-14 00:04:04 -0700

    adds create route and action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,15 @@ class BooksController < ApplicationController
     books = Book.all
     render json: books.as_json
   end
+
+  def create
+    book = Book.new(
+      title: params[:title],
+      author: params[:author],
+      year: params[:year],
+      image: params[:image]
+    )
+    book.save
+    render json: book.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "/books" => "books#index"
+  post "/books" => "books#create"
   
 end
